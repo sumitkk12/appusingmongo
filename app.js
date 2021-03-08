f6943f893d99b2126adc71a9cb166d80bb5e3aa7
sumitkk12 <69508514+sumitkk12@users.noreply.github.com>
2021-03-08 17:35:24 +0530

    added cors library

--- app.js
+++ app.js
@@ -17,17 +17,7 @@ const student = require('./db/dbmodel/student')
 app.use(express.json())
 
 app.use(cors());
-
-app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-origin", "*")
-    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD,OPTIONS, PUT, PATCH, DELETE")
-    res.header("Access-Control-Allow-Headers", "Origin",
-    "X-Requested-With", "Content-Type", "Accept")
-    next()
-    })
-
-    //app.use('/',(req,res)=>res.send("hello from express.This is Sumit Kulkarni"));
-
+    //app.use('/',(req,res)=>res.send("hello from express.This is Sumit Kulkarni"))
     //posts method for creating a new student class
 app.post('/myclass', (req, res) => {
     (new myclass({'className': req.body.className, 'numberOfStudents' : req.body.numberOfStudents}))
