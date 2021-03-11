f4067eb9de909571bc85f634e004088d53775643
sumitkk12 <69508514+sumitkk12@users.noreply.github.com>
2021-03-11 14:27:49 +0530

    added async await

--- app.js
+++ app.js
@@ -19,34 +19,30 @@ app.use(express.json())
 app.use(cors());
     //app.use('/',(req,res)=>res.send("hello from express.This is Sumit Kulkarni"))
     //posts method for creating a new student class
-app.post('/myclass', (req, res) => {
-    (new myclass({'className': req.body.className, 'numberOfStudents' : req.body.numberOfStudents}))
+app.post('/myclass', async(req, res) => {
+   const result =  await (new myclass({'className': req.body.className, 'numberOfStudents' : req.body.numberOfStudents}))
     .save()
-    .then((myclass) => res.send(myclass))
-    .catch((error) => console.log(error))
+    res.json(result);
     })
 
     // register a student into a class
-app.post('/myclass/:myclassId/students', (req, res) =>{
-    (new student ({ 'name': req.body.name, 'lastName': req.body.lastName, '_classId' : req.params.myclassId }))
+app.post('/myclass/:myclassId/students', async(req, res) =>{
+    const result = await (new student ({ 'name': req.body.name, 'lastName': req.body.lastName, '_classId' : req.params.myclassId }))
     .save()
-    .then((student) => res.send(student))
-    .catch((error) => console.log(error))
+    res.json(result);
     })
 
     //read all classes
-app.get('/myclass', (req, res) =>{
-    myclass.find({})
-    .then(myclass =>res.send(myclass))
-    .catch((error) => console.log(error))
+app.get('/myclass', async(req, res) =>{
+   const result =  await myclass.find({})
+    res.json(result);
     })
 
 //get one student class
-app.get('/myclass/:myclassId', (req, res) =>{
-    myclass.findOne( { _id: req.params.myclassId })
-    .then(myclass =>res.send(myclass))
-    .catch((error) => console.log(error))
-    })    
+app.get('/myclass/:myclassId', async(req, res) =>{
+    const result = await myclass.findOne( { _id: req.params.myclassId })
+    res.json(result)
+   })    
 
 
 //get all students from this classId ok then
@@ -68,39 +64,48 @@ app.get('/myclass/:myclassId/students/:studentId', (req, res) =>{
 
     //update student class
 //$set: req.body for auto update of values in the body
-app.patch('/myclass/:myclassId', (req, res) =>{
-    myclass.findOneAndUpdate({ '_id' : req.params.myclassId }, {$set: req.body})
-    .then((myclass) => res.send(myclass))
-    .catch((error) => console.log(error))
+app.patch('/myclass/:myclassId', async(req, res) =>{
+  let result= await myclass.findOneAndUpdate({ '_id' : req.params.myclassId }, {$set: req.body})
+    res.json(result);
     })
 
-    //update student information
-app.patch('/myclass/:myclassId/students/:studentId', (req, res) => {
-    student.findOneAndUpdate({ '_id': req.params.myclassId, _id: req.params.studentId }, { $set: req.body })
-    .then((student) => res.send(student))
-    .catch((error) => console.log(error))
-    })
+//update student information
+app.patch('/myclass/:myclassId/students/:studentId', async(req, res) => {
+  let result11 =  await student.findOneAndUpdate({ '_id': req.params.myclassId, _id: req.params.studentId }, { $set: req.body })
+  res.send("updated the student info"); 
+ })
 
-    //deleting
+
+
+
+   
+        //deleting
     //deleteStudents = (classId) => {student.deleteMany({_id:classId})}
 
-    app.delete('/myclass/:myclassId', (req, res) =>{
-        const deleteStudents = (myclass) =>{
-        student.deleteMany({ '_id': req.params.myclassId})
-        .then(() => myclass)
-        .catch((error) => console.log(error))
-        }
-        myclass.findByIdAndDelete( { '_id': req.params.myclassId})
-        .then((myclass) => res.send(deleteStudents(myclass)))
-        .catch((error) => console.log(error))
+    app.delete('/myclass/:myclassId', async(req, res) =>{
+        const deleteStudents = async(myclass) =>{
+        await student.deleteMany({ '_id': req.params.myclassId},()=>{
+            return myclass;
         })
-
+        
+        //.then(() => myclass)
+        //.catch((error) => console.log(error))
+        }
+        await myclass.findByIdAndDelete( { '_id': req.params.myclassId},(err,doc)=>{
+            deleteStudents(doc);
+            res.send(doc);
+        })
+        
+        })
+       // .then((myclass) => res.send(deleteStudents(myclass)))
+        //.catch((error) => console.log(error))
         //delete student info
-app.delete('/myclass/:myclassId/students/:studentId', (req, res) => {
-    student.findOneAndDelete({ _id: req.params.studentId, _classId: req.params.myclassId }).then((student) => res.send(student))
-    .catch((error) => console.log(error))
+app.delete('/myclass/:myclassId/students/:studentId', async(req, res) => {
+     await student.findOneAndDelete({ _id: req.params.studentId, _classId: req.params.myclassId },(docs)=>{
+        res.send("student successfully deleted");
+     })
+    
     })
-
 
 
 
